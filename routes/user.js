5f224c22f9f02963f32c41c2ba2f777b0fbfb566
melakuzeleke <116151311+melbatestgithub@users.noreply.github.com>
2024-03-06 09:33:33 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -56,7 +56,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new;
   try {
     const users = query
-      ? await User.find().sort({ _id: -1 }).limit(4)
+      ? await User.find({isAdmin:false}).sort({ _id: -1 }).limit(4)
       : await User.find();
     res.status(200).json(users);
   } catch (err) {
