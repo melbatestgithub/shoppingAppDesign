5ae50b9189442c83cc19573a239d1bff833436ba
melakuzeleke <116151311+melbatestgithub@users.noreply.github.com>
2024-03-18 21:37:37 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -45,7 +45,6 @@ router.post("/login", async (req, res) => {
         isAdmin: user.isAdmin,
       },
       process.env.JWT_SEC,
-      { expiresIn: "3d" }
     );
     const { password, ...others } = user._doc;
     res.status(200).json({ ...others, accessToken });
